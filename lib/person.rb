cce07c6eaf11c437496358e49691e3c0ddd499e7
Xiaoyi Liu <lxy878@gmail.com>
2020-03-14 03:11:48 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,8 +2,8 @@ class Person
   #your code here
   attr_accessor :name,:birthday,:hair_color, :eye_color, :height,:weight,:handed,:complexion,:t_shirt_size,:wrist_size,:glove_size,:pant_length,:pant_width
 
-  def initialze(attributes)
+  def initialize(attributes)
     attributes.each {|key, value| self.send("#{key}=", value)}
   end
-  
+
 end
